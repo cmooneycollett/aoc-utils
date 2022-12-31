a58fa304bc07251c323c206c9b4c294f7fad34c5
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2022-12-31 13:24:52 +1000

    Add CardinalDirection to cartography module

---
+++ src/cartography/cardinaldirection.rs
@@ -0,0 +1,40 @@
+/// Represents the four compass cardinal directions.
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum CardinalDirection {
+    North,
+    East,
+    South,
+    West
+}
+
+impl CardinalDirection {
+    /// Determines the cardinal direction resulting from rotating clockwise by 90 degrees the
+    /// specified number of times.
+    pub fn rotate90_clockwise(&self, n: u64) -> CardinalDirection {
+        let mut new_direction = *self;
+        for _ in 0..n {
+            new_direction = match new_direction {
+                CardinalDirection::North => CardinalDirection::East,
+                CardinalDirection::East => CardinalDirection::South,
+                CardinalDirection::South => CardinalDirection::West,
+                CardinalDirection::West => CardinalDirection::North,
+            }
+        }
+        new_direction
+    }
+
+    /// Determines the cardinal direction resulting from rotating counter-clockwise by 90 degrees
+    /// the specified number of times.
+    pub fn rotate90_counterclockwise(&self, n: u64) -> CardinalDirection {
+        let mut new_direction = *self;
+        for _ in 0..n {
+            new_direction = match new_direction {
+                CardinalDirection::North => CardinalDirection::West,
+                CardinalDirection::East => CardinalDirection::North,
+                CardinalDirection::South => CardinalDirection::East,
+                CardinalDirection::West => CardinalDirection::South,
+            }
+        }
+        new_direction
+    }
+}
