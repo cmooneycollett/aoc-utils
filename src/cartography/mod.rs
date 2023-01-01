bfe75bafa361c8a3f63da389d3667d7d0db996d3
Connor Mooney-Collett <connor.mooneycollett@gmail.com>
2023-01-01 11:25:54 +1000

    Add MinMax2D to cartography - Update Point2D with get and set method for fields

--- src/cartography/mod.rs
+++ src/cartography/mod.rs
@@ -1,5 +1,7 @@
 mod cardinaldirection;
+mod minmax2d;
 mod point2d;
 
 pub use self::cardinaldirection::CardinalDirection;
+pub use self::minmax2d::MinMax2D;
 pub use self::point2d::Point2D;
--- src/cartography/point2d.rs
+++ src/cartography/point2d.rs
@@ -10,6 +10,26 @@ impl Point2D {
         Self { x, y }
     }
 
+    /// Gets the value of the "x" field.
+    pub fn x(&self) -> i64 {
+        self.x
+    }
+
+    /// Sets the value of the "x" field.
+    pub fn set_x(&mut self, x: i64) {
+        self.x = x;
+    }
+
+    /// Gets the value of the "y" field.
+    pub fn y(&self) -> i64 {
+        self.y
+    }
+
+    /// Sets the value of the "y" field.
+    pub fn set_y(&mut self, y: i64) {
+        self.y = y;
+    }
+
     /// Shifts the point by the given deltas in the x- and y-directions.
     pub fn shift(&mut self, dx: i64, dy: i64) {
         self.x += dx;
